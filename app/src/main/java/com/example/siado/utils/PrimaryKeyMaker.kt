a4c9eccf017a7f4fa078581b74ef8cd438cf1d8f
VictorImm <victorimmanuelss@gmail.com>
2023-10-16 22:15:48 +0700

    [NEW]

--- app/src/main/java/com/example/siado/utils/PrimaryKeyMaker.kt
+++ app/src/main/java/com/example/siado/utils/PrimaryKeyMaker.kt
@@ -4,9 +4,8 @@ import com.example.siado.data.DateTime
 
 object PrimaryKeyMaker {
 
-    fun make(dateTime: DateTime): Int {
-        val key = "${dateTime.date}${dateTime.mon}${dateTime.year}${dateTime.hour}${dateTime.min}${dateTime.sec}"
-        return (key.toInt())
+    fun make(dateTime: DateTime): String {
+        return "${dateTime.date}${dateTime.mon}${dateTime.year}${dateTime.hour}${dateTime.min}${dateTime.sec}"
     }
 
 }
